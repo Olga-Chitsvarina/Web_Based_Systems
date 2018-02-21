91f3c3435c2eed006fdae2695a26e28e3e8961e2
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2018-02-21 15:45:38 -0700

    Add files via upload

--- Project_Online_Chat/public/client.js
+++ Project_Online_Chat/public/client.js
@@ -1,11 +1,13 @@
 // shorthand for $(document).ready(...)
 $(function() {
     var socket = io();
+
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
-	$('#m').val('');
+	socket.emit('chat', $('#messaging-field').val());
+	$('#messaging-field').val('');
 	return false;
     });
+
     socket.on('chat', function(msg){
 	$('#messages').append($('<li>').text(msg));
     });
