7fe9d74aefeddf6d19035e9e83fdb3a43ee4871d
Olga-Chitsvarina <olga.chitsvarina@gmail.com>
2018-02-23 23:17:22 -0700

    Add files via upload

--- Project_Online_Chat/public/client.js
+++ Project_Online_Chat/public/client.js
@@ -1,14 +1,22 @@
-// shorthand for $(document).ready(...)
-$(function() {
-    var socket = io();
+/* Shorthand for $(document).ready(...),
+ The function will be executed only when the document has finished loading.
+ This function contains JQuery Code inside.
+ JQuery works on client side and does not require page reloading*/
 
-    $('form').submit(function(){
-	socket.emit('chat', $('#messaging-field').val());
-	$('#messaging-field').val('');
-	return false;
+$(function() {
+
+    var socket = io();                                      // It does auto-discovery (find socket), allows bidirectional
+                                                            // communication between the client and a server.
+
+    // Send info to the server:
+    $('form').submit(function(){                            //  If form is submit, perform the action
+        socket.emit('chat', $('#messaging-field').val());   // Transmit message: type: 'chat', content: field text
+            $('#messaging-field').val('');                  // Update the field with the empty string
+            return false;                                   // Finish submission
     });
 
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+    // Get info from the server
+    socket.on('chat', function(msg){                        // When get info from the server, do the following
+	    $('#messages-list').append($('<li>').text(msg));    // Find list of messages, add another one with msg text
     });
 });
